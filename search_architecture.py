acb673e796481553cd392c1588870614ab95e1bc
Abdul Rehman <amaizingboy197@gmail.com>
2023-02-19 17:57:39 +0500

    modified

--- search_architecture.py
+++ search_architecture.py
@@ -424,6 +424,7 @@ def get_desired_dataset(args):
   normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
   train_transform= transforms.Compose([
+            transforms.Resize((224,224))
             transforms.RandomResizedCrop(224),
             transforms.RandomHorizontalFlip(),
             transforms.ColorJitter(
@@ -436,7 +437,7 @@ def get_desired_dataset(args):
           ])
 
   test_transform = transforms.Compose([
-      transforms.Resize(256),
+      transforms.Resize((224,224)),
       transforms.CenterCrop(224),
       transforms.ToTensor(),
       normalize,
