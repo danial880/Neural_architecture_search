5b37179c7165a420bc044000a794df6afbbcf394
Abdul Rehman <amaizingboy197@gmail.com>
2023-02-21 15:41:21 +0500

    modeified

--- NetworkMix.py
+++ NetworkMix.py
@@ -158,3 +158,76 @@ class NetworkMix_ImageNet(nn.Module):
     logits = self.classifier(out.view(out.size(0),-1))
     
     return logits
+
+
+class NetworkMix_arch(nn.Module):
+
+  def __init__(self, C, num_classes, layers, mixnet_code, k_size, im_size):
+    super(NetworkMix_arch, self).__init__()
+    self._layers = layers
+    s = [1,1,1]
+    for i in range(im_size[0]//64):
+        s[i]=2
+    self.stem0 = nn.Sequential(
+      nn.Conv2d(3, C // 2, kernel_size=3, stride=s[0], padding=1, bias=False),
+      nn.BatchNorm2d(C // 2),
+      nn.ReLU(inplace=True),
+      nn.Conv2d(C // 2, C, 3, stride=s[1], padding=1, bias=False),
+      nn.BatchNorm2d(C),
+    )
+    
+    self.stem1 = nn.Sequential(
+      nn.ReLU(inplace=True),
+      nn.Conv2d(C, C, 3, stride=s[2], padding=1, bias=False),
+      nn.BatchNorm2d(C),
+    )
+
+ 
+    C_prev, C_curr = C, C
+    
+    self.mixlayers = nn.ModuleList()
+    reduction_prev = False
+    
+    for i in range(layers):
+      if i in [layers//3, 2*layers//3]:
+        C_curr *= 2
+        reduction = True
+      else:
+        reduction = False
+        
+      stride = 2 if reduction else 1
+      
+      if k_size[i]==3:
+        pad=1
+      elif k_size[i]==5: 
+        pad=2
+      else:
+        pad=3
+
+      
+      if mixnet_code[i] == 0:
+        mixlayer = SepConv(C_prev, C_curr, kernel_size=k_size[i], stride=stride, padding=pad, affine=True)
+      else:
+        mixlayer = Conv(C_prev, C_curr, kernel_size=k_size[i], stride=stride, padding=pad, affine=True)
+
+      reduction_prev = reduction
+        
+      self.mixlayers += [mixlayer]
+      C_prev = C_curr
+
+    self.global_pooling = nn.AvgPool2d(7)
+    self.classifier = nn.Linear(C_prev, num_classes)
+
+  def forward(self, x):
+    
+    x = self.stem0(x)
+    x = self.stem1(x)
+    
+    for i, mixlayer in enumerate(self.mixlayers):
+      x = mixlayer(x)
+        
+    out = self.global_pooling(x)
+    logits = self.classifier(out.view(out.size(0),-1))
+    
+    return logits
+
