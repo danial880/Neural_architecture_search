fa441effb16b2b0589550e38fc4004f5ac725f3e
Abdul Rehman <amaizingboy197@gmail.com>
2023-02-21 15:41:45 +0500

    modeified

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import torch
 import shutil
 import torchvision.transforms as transforms
 from torch.autograd import Variable
+import torchvision
 
 
 class AvgrageMeter(object):
@@ -93,6 +94,7 @@ def save_checkpoint(state, is_best, save):
 
 
 def save(model, model_path):
+  print(model_path)
   torch.save(model.state_dict(), model_path)
 
 
@@ -115,3 +117,122 @@ def create_exp_dir(path, scripts_to_save=None):
       shutil.copyfile(script, dst_file)
 
 
+def data_load_transforms(args):
+
+
+  normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+  train_transform= transforms.Compose([
+            #transforms.Resize((224,224)),
+            transforms.RandomResizedCrop(args.input_shape),
+            transforms.RandomHorizontalFlip(),
+            transforms.ColorJitter(
+              brightness=0.4,
+              contrast=0.4,
+              saturation=0.4,
+              hue=0.2),
+            transforms.ToTensor(),
+            normalize,
+          ])
+
+  test_transform = transforms.Compose([
+      # transforms.Resize((224,224)),
+      transforms.Resize(args.input_shape),
+      transforms.CenterCrop(args.input_shape),
+      transforms.ToTensor(),
+      normalize,
+    ])
+  if args.dataset == 'cifar10':
+    train_data = torchvision.datasets.CIFAR10(root=args.data_dir, train=True,
+                                        download=True, transform=train_transform)
+  
+    test_data = torchvision.datasets.CIFAR10(root=args.data_dir, train=False,
+                                           download=True, transform=test_transform)
+    
+    total_classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+   
+  if args.dataset == 'STL10':
+    train_data = torchvision.datasets.STL10(root=args.data_dir, train=True,
+                                        download=True, transform=train_transform)
+   
+    test_data = torchvision.datasets.STL10(root=args.data_dir, train=False,
+                                           download=True, transform=test_transform)
+   
+    total_classes = ['airplane','bird','car','cat','deer','dogs','horse','monkey','ship','truck']
+   
+  
+  if args.dataset == 'DTD':
+
+    train_data = torchvision.datasets.DTD(root=args.data_dir, split='train',download=True, transform=train_transform)
+   
+    test_data = torchvision.datasets.DTD(root=args.data_dir, split='val',
+                                           download=True, transform=test_transform)
+    total_classes = ['banded', 'blotchy', 'braided', 'bubbly', 'bumpy', 'chequered', 'cobwebbed', 'cracked', 'crosshatched',  
+              'crystalline', 'dotted', 'fibrous', 'flecked', 'frothy', 'gauzy', 'grid', 'grooved', 'herringbone',   
+               'interlaced', 'knitted', 'lacelike', 'lined', 'marbled', 'matted', 'meshed', 'paisley', 'patterned',  
+               'plaid', 'polka-dotted', 'porous', 'radial', 'ribbed', 'scaled', 'smeared', 'spangled', 'speckled',
+               'stippled', 'striped', 'studded', 'swirly', 'veined', 'waffled', 'woven', 'wrinkled', 'zigzagged',
+                'homogeneous', 'non-homogeneous']
+    
+  if args.dataset == 'cifar100':
+    
+    train_data = torchvision.datasets.CIFAR100(root=args.data_dir, train=True,
+                                        download=True, transform=train_transform)
+   
+    test_data = torchvision.datasets.CIFAR100(root=args.data_dir, train=False,
+                                           download=True, transform=test_transform)
+   
+    total_classes =['apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle', 'bicycle', 'bottle', 'bowl',            
+    'boy', 'bridge', 'bus', 'butterfly', 'camel', 'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee',            
+    'clock', 'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur', 'dolphin', 'elephant', 'flatfish',            
+    'forest', 'fox', 'girl', 'hamster', 'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard', 'lion',            
+    'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse', 'mushroom', 'oak_tree', 'orange',            
+    'orchid', 'otter', 'palm_tree', 'pear', 'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine',            
+    'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose', 'sea', 'seal', 'shark', 'shrew', 'skunk',            
+    'skyscraper', 'snail', 'snake', 'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table',            
+    'tank', 'telephone', 'television', 'tiger', 'tractor', 'train', 'trout', 'tulip', 'turtle', 'wardrobe',            
+    'whale', 'willow_tree', 'wolf', 'woman', 'worm']
+
+    
+    
+  if args.dataset == 'Food101':
+    train_data = torchvision.datasets.Food101(root=args.data_dir, split='train',download=True, transform=train_transform)
+   
+    test_data = torchvision.datasets.Food101(root=args.data_dir, split='val',download=True, transform=test_transform)
+
+    total_classes = ['macarons', 'french_toast', 'lobster_bisque', 'prime_rib', 'pork_chop', 'guacamole', 'baby_back_ribs', 'mussels', 'beef_carpaccio', 'poutine',
+                    'hot_and_sour_soup', 'seaweed_salad', 'foie_gras', 'dumplings', 'peking_duck', 'takoyaki', 'bibimbap', 'falafel', 'pulled_pork_sandwich', 'lobster_roll_sandwich',
+                    'carrot_cake', 'beet_salad', 'panna_cotta', 'donuts', 'red_velvet_cake', 'grilled_cheese_sandwich', 'cannoli', 'spring_rolls', 'shrimp_and_grits',
+                    'clam_chowder','omelette', 'fried_calamari', 'caprese_salad', 'oysters', 'scallops', 'ramen', 'grilled_salmon', 'croque_madame', 'filet_mignon',
+                    'hamburger', 'spaghetti_carbonara', 'miso_soup', 'bread_pudding', 'lasagna', 'crab_cakes', 'cheesecake', 'spaghetti_bolognese', 'cup_cakes', 'creme_brulee',
+                    'waffles', 'fish_and_chips', 'paella', 'macaroni_and_cheese', 'chocolate_mousse', 'ravioli', 'chicken_curry', 'caesar_salad', 'nachos', 'tiramisu', 'frozen_yogurt',
+                    'ice_cream', 'risotto', 'club_sandwich', 'strawberry_shortcake', 'steak', 'churros', 'garlic_bread', 'baklava', 'bruschetta', 'hummus', 'chicken_wings',
+                    'greek_salad', 'tuna_tartare', 'chocolate_cake', 'gyoza', 'eggs_benedict', 'deviled_eggs', 'samosa', 'sushi', 'breakfast_burrito', 'ceviche', 'beef_tartare',
+                    'apple_pie', '.DS_Store', 'huevos_rancheros', 'beignets', 'pizza', 'edamame', 'french_onion_soup', 'hot_dog', 'tacos', 'chicken_quesadilla', 'pho', 'gnocchi',
+                    'pancakes', 'fried_rice', 'cheese_plate', 'onion_rings', 'escargots', 'sashimi', 'pad_thai', 'french_fries']
+
+  if args.dataset == 'flower102':
+
+    train_data = torchvision.datasets.Flowers102(root=args.data_dir, split='train',
+                                        download=True, transform=train_transform)
+   
+    test_data = torchvision.datasets.Flowers102(root=args.data_dir, split='val',
+                                           download=True, transform=test_transform)
+
+    total_classes =["pink primrose", "hard-leaved pocket orchid", "canterbury bells", "sweet pea", "english marigold", 
+    "tiger lily", "moon orchid", "bird of paradise", "monkshood", "globe thistle", "snapdragon",
+     "colt's foot", "king protea", "spear thistle", "yellow iris", "globe-flower", "purple coneflower", "peruvian lily",
+    "balloon flower", "giant white arum lily", "fire lily", "pincushion flower", "fritillary", "red ginger", "grape hyacinth",
+    "corn poppy", "prince of wales feathers", "stemless gentian", "artichoke", "sweet william", "carnation", "garden phlox", 
+    "love in the mist", "mexican aster", "alpine sea holly", "ruby-lipped cattleya", "cape flower", "great masterwort", 
+    "siam tulip", "lenten rose", "barbeton daisy", "daffodil", "sword lily", "poinsettia", "bolero deep blue", "wallflower", 
+    "marigold", "buttercup", "oxeye daisy", "common dandelion", "petunia", "wild pansy", "primula", "sunflower", "pelargonium", 
+    "bishop of llandaff", "gaura", "geranium", "orange dahlia", "pink-yellow dahlia", "cautleya spicata", "japanese anemone", 
+    "black-eyed susan", "silverbush", "californian poppy", "osteospermum", "spring crocus", "bearded iris", "windflower", "tree poppy", 
+    "gazania", "azalea", "water lily", "rose", "thorn apple", "morning glory", "passion flower", "lotus", "toad lily", "anthurium", 
+    "frangipani", "clematis", "hibiscus", "columbine", "desert-rose", "tree mallow", "magnolia", "cyclamen", "watercress", 
+    "canna lily", "hippeastrum", "bee balm", "ball moss", "foxglove", "bougainvillea", "camellia", "mallow", "mexican petunia", 
+    "bromelia", "blanket flower", "trumpet creeper", "blackberry lily"]
+  
+  
+  return train_data, test_data, total_classes
